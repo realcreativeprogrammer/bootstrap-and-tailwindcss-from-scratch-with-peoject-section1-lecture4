ecc8c42b81884d51938312b55bf40c6c57916c94
realcreativeprogrammer <realcreativeprogrammer@gmail.com>
2021-10-21 20:03:11 +0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        test:{
+          light:'#1dd1a1',
+          dark:'#0abde3',
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
